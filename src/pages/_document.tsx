427dbdaa7a2da0181c99968af355b6f77695fc39
DevJpedroASeco <dev.jpedro@gmail.com>
2022-07-01 00:29:38 -0300

    Adição do favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,6 +8,7 @@ export default class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
           <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700;900&display=swap" rel="stylesheet" />
+          <link rel="icon" type="image/x-icon" href="/img/poke-ball.png" />
         </Head>
         <body>
           <Main />
